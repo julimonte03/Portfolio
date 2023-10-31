2a6fad5d5d4674a5103316e9f5e323720847de6b
Julian Monte <julianmonte2003@gmail.com>
2023-10-30 23:30:43 -0300

    hero section and personal info

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const header = document.querySelector("header");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+    header.style.overflow = "visible"; // Mostrar contenido desbordado
+});
 
 cerrar.addEventListener("click", () =>{
     nav.classList.remove("visible");
+    header.style.overflow = "hidden"; // Ocultar contenido desbordado
 })
